361c198477ea7d35b8e9999a716e786be77e4f99
Eric Easley <eric@frontrowed.com>
2016-01-26 09:53:49 -0800

    Add flow to demo

--- index.jsx
+++ index.jsx
@@ -1,13 +1,22 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
+/* @flow */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+type FlowString = 'flow' | 'Flow'
+
 export class App extends React.Component {
-	render() {
+  render(): ReactElement {
 		return (
-      <div>Simple React + Babel + Bootstrap + Webpack </div>
+      <div>Simple React + Babel + Bootstrap + Webpack + {this.typeChecks()}</div>
 		);
-	}
+  }
+  typeChecks(): FlowString {
+    return 'flow'
+  }
+  /* fails(): FlowString { */
+  /* return 'foo' */
+  /* } */
 }
 
 ReactDOM.render(<App/>, document.querySelector("#myApp"));
